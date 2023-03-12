45cb58bdf1f147cb7fbcd3c41ffc557fb0762208
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-03-12 18:41:02 +0200

    Added window draw and clear events to event loop

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,7 @@
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-
-fn match_window_events(event: &WindowEvent, control_flow: &mut ControlFlow) {
-    match event {
-        WindowEvent::CloseRequested
-        | WindowEvent::KeyboardInput {
-            input:
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                    ..
-                },
-            ..
-        } => *control_flow = ControlFlow::Exit,
-        _ => {}
-    }
-}
-
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
-fn run() {
-    // Initialize env_logger
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
-        } else {
-            env_logger::init();
-        }
-    }
-
-    // Initialize event loop and window
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    // Add canvas for web support
-    #[cfg(target_arch = "wasm32")]
-    {
-        use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(450, 400));
-
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| {
-                let dst = doc.get_element_by_id("wasm-example")?;
-                let canvas = web_sys::Element::from(window.canvas());
-                dst.append_child(&canvas).ok()?;
-                Some(())
-            })
-            .expect("Couldnt't append canvas to document body.");
-    }
-
-    // Run the event_loop
-    event_loop.run(move |event, _, mut control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match_window_events(&event, &mut control_flow),
-        _ => {}
-    });
-}
+mod lib;
+use lib::run;
 
 #[allow(dead_code)]
 fn main() {
-    run();
+    pollster::block_on(run());
 }
